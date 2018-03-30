f85f3aeaa06f2c50cc488da23f4f4d8c65d29fdb
zed15600 <zed15600@gmail.com>
2018-03-30 01:14:06 -0500

    Cambiando usernames y passwords quemadas por variables de entorno

--- proyectoGPS/config/initializers/omniauth.rb
+++ proyectoGPS/config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '833569790780-k3a949cjcl5s1givu21ua7e1lo2fb1sh.apps.googleusercontent.com', 'PIauK8lAMrZW2ucok7rdL86O', {
+  provider :google_oauth2, ENV["OMNIAUTH_ID"], ENV["OMNIAUTH_SECRET"], {
     client_options: {
       ssl: {
         ca_file: Rails.root.join("cacert.pem").to_s
